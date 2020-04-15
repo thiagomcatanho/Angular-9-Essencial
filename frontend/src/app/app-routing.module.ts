4186fe32d36042a518320e49fed8779245f614e8
Thiago Martins <catanho333@gmail.com>
2020-04-14 23:17:09 -0300

    Criação da Navegação

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './view/home/home.component';
+import { ProductCrudComponent } from './view/product-crud/product-crud.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  }, {
+    path: "products",
+    component: ProductCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
